62ee2106e322bdff2ae8680deebe776ad17a4c83
ifer-itcast <yangkangkang@itcast.cn>
2021-02-19 10:24:06 +0800

    规则校验

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <el-card class="login-card">
-      <el-form :model="loginFormData" style="margin-top: 50px;">
-        <el-form-item>
+      <el-form :model="loginFormData" :rules="loginFormRules" style="margin-top: 50px;">
+        <el-form-item prop="mobile">
           <el-input v-model="loginFormData.mobile" placeholder="请输入您的手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input v-model="loginFormData.password" placeholder="请输入您的密码"></el-input>
         </el-form-item>
         <el-form-item>
@@ -17,7 +17,6 @@
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
@@ -25,11 +24,23 @@ export default {
       // 数据
       loginFormData: {
         mobile: '',
-        password: ''
+        password: '',
       },
-    }
-  }
-}
+      // 规则
+      loginFormRules: {
+        // !key 代表要校验的字段名
+        mobile: [
+          { required: true, message: '手机号不能为空', trigger: 'blur' },
+          { pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: 'blur' },
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 6, max: 16, message: '密码的长度为 6~16 位', trigger: 'blur' },
+        ],
+      },
+    };
+  },
+};
 </script>
 
 <style>
@@ -41,7 +52,7 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.login-card{
+.login-card {
   width: 440px;
   height: 300px;
   background-color: #fff;
